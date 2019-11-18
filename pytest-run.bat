15f475caad95822cfe2d1ced645bcd5519e1758b
damiantgordon <57894391+damiantgordon@users.noreply.github.com>
2019-11-18 09:30:46 +0000

    Add files via upload

---
+++ pytest-run.bat
@@ -0,0 +1,5 @@
+@echo off
+
+set /p FILENAME="Enter filename: "
+
+py.test c:\Users\damian.gordon\AppData\Local\Programs\Python\Python35-32\%FILENAME%.py
---
+++ pytest-setup.bat
@@ -0,0 +1,11 @@
+@echo off
+
+SET PATH=%PATH%;c:\Users\damian.gordon\AppData\Local\Programs\Python\Python35-32
+
+SET PATH=%PATH%;c:\Users\damian.gordon\AppData\Local\Programs\Python\Python35-32\Scripts
+
+path
+
+pip install -U pytest
+
+pytest --version
